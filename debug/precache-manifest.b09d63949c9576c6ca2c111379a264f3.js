864a7111d17cfc02caae5c22c849d20f930652f3
shadc <shadc@gmx.com>
2019-05-04 12:15:37 -0700

    Fix issues with public folder, add relative paths to build items.

---
+++ debug/precache-manifest.b09d63949c9576c6ca2c111379a264f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "2e85b3540bcd1092ebb6",
+    "url": "./static/js/main.72a67b16.chunk.js"
+  },
+  {
+    "revision": "5a0e7a1ced696af2f680",
+    "url": "./static/js/2.d00b9fdf.chunk.js"
+  },
+  {
+    "revision": "2e85b3540bcd1092ebb6",
+    "url": "./static/css/main.afd73dc0.chunk.css"
+  },
+  {
+    "revision": "fb6d93f0789178099e8939b22706fb25",
+    "url": "./index.html"
+  }
+];
