d6846b99695ec3d5f0e568ffd6c01bfa7b955c72
shadc <shadc@gmx.com>
2026-05-02 15:15:40 -0700

    Update to gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    base: "/my3d-story-tour/",
     plugins: [react()],
     server: {
         host: "0.0.0.0",
